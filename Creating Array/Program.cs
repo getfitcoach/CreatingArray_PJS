0ca2404205e685de120a6a0d3d15529c95aec8b4
Paul <gowild45@gmail.com>
2020-01-06 13:28:28 -0500

    first commit

---
+++ Creating Array/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //print all of them
+            string[] namearray = new string[5];
+
+            for (int counter1 = 0; counter1 < namearray.Length; counter1++)
+       
+                {
+                    Console.WriteLine("Please enter a name");
+                    namearray[counter1] = Console.ReadLine();
+                    // Console.WriteLine(namearray[0]);
+                }
+
+
+            //Console.WriteLine("please enter a name");
+           // namearray[1] = Console.ReadLine();
+            //Console.WriteLine(namearray[1]);
+
+
+            //Console.WriteLine("next name please");
+           // namearray[2] = Console.ReadLine();
+            //Console.WriteLine(namearray[2]);
+
+
+           // Console.WriteLine("another name please");
+           // namearray[3] = Console.ReadLine();
+            //Console.WriteLine(namearray[3]);
+
+
+           // Console.WriteLine("last name please");
+           // namearray[4] = Console.ReadLine();
+            //Console.WriteLine(namearray[4]);
+        
+
+
+
+            for (int counter = 0; counter < namearray.Length; counter++)
+            {
+                Console.WriteLine(namearray[counter]);
+
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
